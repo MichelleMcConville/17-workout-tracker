ed16e9b80fbf63943bafc21058a931090256b1b4
Michelle McConville <mchelmconvl@gmail.com>
2021-01-17 17:00:51 -0600

    added WorkoutSchema

--- models/workout.js
+++ models/workout.js
@@ -42,3 +42,10 @@ const WorkoutSchema = new Schema({
     }
   }]
 });
+
+WorkoutSchema.virtual("totalDuration").get(function() {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercises.duration;
+  }, 0);
+});
+
