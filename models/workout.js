9d9e0c69862b30cc4a1592c823245a9a39a8b1eb
Michelle McConville <mchelmconvl@gmail.com>
2021-01-17 17:10:10 -0600

    changed formatting

--- models/workout.js
+++ models/workout.js
@@ -1,53 +1,57 @@
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
   day: {
     type: Date,
-    default: () => new Data(),
+    default: Date.now,
   },
 
-  exercises: [{
-    type: {
-      type: String,
-      trim: true,
-      required: "Enter exercise type",
-    },
+  exercises: [
+    {
+      type: 
+      {
+        type: String,
+        trim: true,
+        required: "Enter exercise type",
+      },
 
-    name: {
-      type: String,
-      trim: true,
-      require: "Enter exercises type",
-    },
+      name: {
+        type: String,
+        trim: true,
+        require: "Enter exercises type",
+      },
 
-    duration: {
-      type: Number,
-      required: "Enter duration",
-    },
+      duration: {
+        type: Number,
+        required: "Enter duration",
+      },
 
-    weight: {
-      type: Number,
-    },
+      weight: {
+        type: Number,
+      },
     
-    reps: {
-      type: Number,
-    },
+      reps: {
+        type: Number,
+      },
 
-    sets: {
-      type: Number,
-    },
+      sets: {
+        type: Number,
+      },
 
-    distance: {
-      type: Number,
+      distance: {
+        type: Number,
+      }
     }
-  }]
+  ]
 });
 
-WorkoutSchema.virtual("totalDuration").get(function() {
-  return this.exercises.reduce((total, exercise) => {
-    return total + exercises.duration;
-  }, 0);
-});
+// WorkoutSchema.virtual("totalDuration").get(function() {
+//   return this.exercises.reduce((total, exercise) => {
+//     return total + exercises.duration;
+//   }, 0);
+// });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
