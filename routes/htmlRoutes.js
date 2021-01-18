740e172eb16cc7b6869fbab7be02c604d6844673
Michelle McConville <mchelmconvl@gmail.com>
2021-01-18 15:52:46 -0600

    added app.get #3

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,7 @@
 // const path = require("path");
 import path from "path";
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
@@ -9,5 +9,8 @@ module.exports = function(app) {
   app.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
-  
-}
+
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
