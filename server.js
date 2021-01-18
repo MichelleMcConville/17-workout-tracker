d432b589c39d261b89e51304d9f04c2bf0110140
Michelle McConville <mchelmconvl@gmail.com>
2021-01-18 15:27:44 -0600

    added mongoose.connect

--- server.js
+++ server.js
@@ -10,3 +10,20 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({  extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI, 
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB Connected!'))
+  .catch((err) => console.error(err));
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
