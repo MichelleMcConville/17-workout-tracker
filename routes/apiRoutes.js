6b694b6fd8d2c3bad10671b7d43fda2583c607e8
Michelle McConville <mchelmconvl@gmail.com>
2021-01-19 09:24:13 -0600

    replaced body, params w/ req & removed ES6 conversion

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,28 +1,26 @@
-// const Workout = require("../models/workout.js");
-import Workout from "../models/workout.js";
+const db = require("../models");
 
 module.exports = function (app) {
-  app.get("/api/workouts", ({ body, params }, res) => {
-    Workout.find({})
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
       .then((data) => { res.json(data); })
       .catch((err) => { res.json(err); });
   });
 
-  app.get("/api/workouts/range", ({ body, params }, res) => {
-    Workout.find({})
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
       .then(data => {res.json(data);})
       .catch(err => {res.json(err);});
   });
 
-  app.post("/api/workouts/:id", ({ body, params }, res) => {
-    const workout = new Workout(req.body);
-    Workout.create(workout)
+  app.post("/api/workouts", ({ body }, res) => {
+   db.Workout.create(body)
       .then(data => {res.json(data);})
       .catch(err => {res.json(err);});
   });
 
   app.put("/api/workouts/:id", ({ body, params }, res) => {
-    Workout.findByIdAndUpdate(params.id,
+    db.Workout.findByIdAndUpdate(params.id,
         { $push: { exercises: body}},
         { new: true, runValidators: true }
       )
